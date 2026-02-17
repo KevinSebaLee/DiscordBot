5ecf6302308dabde8dbcb28bed6f65a400ffc136
KevinSebaLee <kevin.lee.sebastian@gmail.com>
2026-02-17 14:02:10 -0300

    feat: Logged bot

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import { Client } from "discord.js"
+import { Client, Intents } from "discord.js"
 import { config } from "dotenv"
 
 config()
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.MESSAGE_CONTENT
+    ],
+})
+
+client.login(process.env.TOKEN)
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`)
+})
